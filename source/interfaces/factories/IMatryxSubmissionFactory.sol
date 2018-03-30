f077a5dfc15e1191f632201d29f2f136822bdf16
Max Howard <tmhoward@ucsd.edu>
2018-03-29 19:23:37 -0700

    Removed source for a sec

--- source/interfaces/factories/IMatryxSubmissionFactory.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.18;
-
-interface IMatryxSubmissionFactory
-{
-	function createSubmission(address tournamentAddress, address roundAddress, string title, address author, bytes32 externalAddress, address[] references, address[] contributors, uint256 timeSubmitted, bool publicallyAccessibleDuringTournament) public returns (address _submissionAddress);
-}
