f1b162bc4b4ee33afdd4c9db76504dabf550996c
Max Howard <tmhoward@ucsd.edu>
2018-04-05 17:32:25 -0700

    git subrepo clone https://github.com/matryx/MatryxExplorer explorer
    subrepo:
      subdir:   "explorer"
      merged:   "b150a24"
    upstream:
      origin:   "https://github.com/matryx/MatryxExplorer"
      branch:   "master"
      commit:   "b150a24"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ explorer/api/controllers/submissionController.js
@@ -0,0 +1,195 @@
+/*
+The Matryx submission controller
+
+authors - sam@nanome.ai
+Nanome 2018
+*/
+
+/*
+Imports
+*/
+const externalApiCalls = require('./gateway/externalApiCalls')
+const ipfsCalls = require('./gateway/ipfsCalls')
+const matryxPlatformCalls = require('./gateway/matryxPlatformCalls')
+
+let submissionController = {}
+
+submissionController.getSubmissionOwnerByAddress = function (_submissionAddress) {
+  return new Promise((resolve, reject) => {
+    matryxPlatformCalls.getSubmissionAuthor(_submissionAddress).then(function (result) {
+      resolve(result)
+    }).catch((err) => {
+      console.log('Not able to retrieve submission owner. ' + err)
+    })
+  })
+}
+
+submissionController.getIpfsDataForSubmission = function (_submissionAddress, _ipfsHash) {
+  return new Promise((resolve, reject) => {
+    console.log('Making gateway call...')
+    ipfsCalls.getIpfsData(_ipfsHash).then(function (result) {
+      console.log(result)
+      resolve(result)
+    }).catch((err) => {
+      console.log('Not able to retrieve submission owner. ' + err)
+    })
+  })
+}
+
+submissionController.uploadToIpfs = function (_files) {
+  return new Promise((resolve, reject) => {
+    console.log('Making gateway call...')
+    ipfsCalls.uploadToIpfs(_files).then(function (result) {
+      console.log(result)
+      resolve(result)
+    }).catch((err) => {
+      console.log('Not able to upload to IPFS. ' + err)
+    })
+  })
+}
+
+submissionController.getSubmissionByAddress = function (_submissionAddress) {
+  console.log('Executing submissionController for getting submission details at: ' + '\'' + _submissionAddress + '\'')
+  return new Promise((resolve, reject) => {
+    let submissionDataCalls = []
+
+    let submissionData = {
+      submissionTitle: '',
+      submissionAddress: '',
+      submissionAuthor: '',
+      submissionId: '',
+      submissionDescription: '',
+      submissionCollaborators: [],
+      submissionReferences: [],
+      submissionContents: [],
+      submissionExternalAddress: '',
+      submissionRewardTotal: 0,
+      submissionSelectedRound: 0,
+      submissionDate: '',
+      parentInfo: []
+    }
+    let parentInfoItem = {
+      currentRound: 0,
+      roundAddress: '',
+      roundMtx: 0,
+      tournamentName: '',
+      tournamentAddress: ''
+    }
+
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionTitle(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionAuthor(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionDescription(_submissionAddress)) // // TODO: MAX
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionContributors(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionReferences(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionContents(_submissionAddress)) // TODO: MAX
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionExternalAddress(_submissionAddress))
+    // submissionDataCalls.push(matryxPlatformCalls.getSubmissionBalance(_submissionAddress))
+    // submissionDataCalls.push(matryxPlatformCalls.getSubmissionSelectedRound(_submissionAddress)) // TODO: MAX what is this
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionTimeSubmitted(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionParentInfo(_submissionAddress))
+          // Promise all for the data inside the submissions
+
+    Promise.all(submissionDataCalls).then(function (values) {
+              // Attach to the submissions Data
+      console.log('Values of submission details after promise all: ' + values)
+      submissionData.submissionTitle = values[0]
+      submissionData.submissionAddress = _submissionAddress
+      submissionData.submissionAuthor = values[1]
+      submissionData.submissionId = ''
+      submissionData.submissionDescription = values[2]
+      submissionData.submissionCollaborators = values[3]
+      submissionData.submissionReferences = values[4]
+      submissionData.submissionContents = values[5] // turn this into a []
+      submissionData.submissionExternalAddress = values[6]
+      // submissionData.submissionRewardTotal = values[5]
+      submissionData.submissionSelectedRound = 0
+      submissionData.submissionDate = values[7]
+      submissionData.parentInfo = values[8]
+            // submissions.submissions = values[10]
+
+      resolve(submissionData)
+    })
+  })
+}
+
+submissionController.uploadJsonAndDescriptionToIPFS = function (jsonContent, description) {
+  return new Promise((resolve, reject) => {
+    console.log('Making gateway call...')
+    ipfsCalls.uploadJsonAndDescriptionToIPFS(jsonContent, description).then(function (result) {
+      console.log(result)
+      resolve(result)
+    }).catch((err) => {
+      console.log('Not able to upload to IPFS. ' + err)
+    })
+  })
+}
+
+/*
+EXPERIMENTAL
+*/
+
+submissionController.getSubmissionByAddressNoIPFS = function (_submissionAddress) {
+  console.log('Executing submissionController for getting submission details at: ' + '\'' + _submissionAddress + '\'')
+  return new Promise((resolve, reject) => {
+    let submissionDataCalls = []
+
+    let submissionData = {
+      submissionTitle: '',
+      submissionAddress: '',
+      submissionAuthor: '',
+      submissionId: '',
+      submissionDescription: '',
+      submissionCollaborators: [],
+      submissionReferences: [],
+      submissionContents: [],
+      submissionExternalAddress: '',
+      submissionRewardTotal: 0,
+      submissionSelectedRound: 0,
+      submissionDate: '',
+      parentInfo: []
+    }
+    let parentInfoItem = {
+      currentRound: 0,
+      roundAddress: '',
+      roundMtx: 0,
+      tournamentName: '',
+      tournamentAddress: ''
+    }
+
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionTitle(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionAuthor(_submissionAddress))
+    // submissionDataCalls.push(matryxPlatformCalls.getSubmissionDescription(_submissionAddress)) // // TODO: MAX
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionContributors(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionReferences(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionContents(_submissionAddress)) // TODO: MAX
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionExternalAddress(_submissionAddress))
+    // submissionDataCalls.push(matryxPlatformCalls.getSubmissionBalance(_submissionAddress))
+    // submissionDataCalls.push(matryxPlatformCalls.getSubmissionSelectedRound(_submissionAddress)) // TODO: MAX what is this
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionTimeSubmitted(_submissionAddress))
+    submissionDataCalls.push(matryxPlatformCalls.getSubmissionParentInfo(_submissionAddress))
+          // Promise all for the data inside the submissions
+
+    Promise.all(submissionDataCalls).then(function (values) {
+              // Attach to the submissions Data
+      console.log('Values of submission details after promise all: ' + values)
+      submissionData.submissionTitle = values[0]
+      submissionData.submissionAddress = _submissionAddress
+      submissionData.submissionAuthor = values[1]
+      submissionData.submissionId = ''
+      submissionData.submissionDescription = 'submission description filler instance'
+      submissionData.submissionCollaborators = values[2]
+      submissionData.submissionReferences = values[3]
+      submissionData.submissionContents = values[4] // turn this into a []
+      submissionData.submissionExternalAddress = values[5]
+      // submissionData.submissionRewardTotal = values[5]
+      submissionData.submissionSelectedRound = 0
+      submissionData.submissionDate = values[6]
+      submissionData.parentInfo = values[7]
+            // submissions.submissions = values[10]
+
+      resolve(submissionData)
+    })
+  })
+}
+
+module.exports = submissionController
---
+++ explorer/docs/source/index.rst
@@ -0,0 +1,21 @@
+MatryxExplorer Documentation
+==========================================
+
+Guide
+^^^^^
+
+.. toctree::
+   :maxdepth: 2
+   :caption: User Documentation
+
+   ipfs
+   help
+   license
+   
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
